900c5730d9569e2cce2e1d26ea34aa5b330e5b9e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-08-05 23:02:15 -0300

    feat: dashboard title

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,20 @@
+import { currentUser } from '@clerk/nextjs';
 import { AlertTopBar } from '@/components/alert-top-bar';
 import { DashboardOptionCard } from '@/components/dashboard/dashboard-option-card';
 import { DashboardPageHeader } from '@/components/dashboard/dashboard-page-header';
 import { Navbar } from '@/components/navbar';
 import { Separator } from '@/components/ui/separator';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const user = await currentUser();
+
   return (
     <div className="flex h-full min-h-screen w-screen flex-col items-center">
       <Navbar />
       <AlertTopBar label="Essa joça tá em BETA. Não fode." />
       <main className="flex h-full w-full max-w-7xl grow flex-col gap-8 p-8">
         <DashboardPageHeader
-          title="Olá, Fulano de Tal"
+          title={`Olá, ${user?.firstName}`}
           subtitle="Algum subtítulo que o nosso qualificado time de desenvolvedores não
             pensou... Quer saber, use sua imaginação!"
         />
