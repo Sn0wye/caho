483209f6fcc6fb7057b0fcf44401bc3679ada0e0
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-08-20 17:02:01 -0300

    fiz UI tbm????

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <SignIn />
+    </div>
+  );
 }
