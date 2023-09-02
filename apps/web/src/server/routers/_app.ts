c656cfb77212a0c0309849fdccd774093c102915
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-09-02 19:09:22 -0300

    refac: delete server

--- apps/web/src/server/routers/_app.ts
+++
@@ -1,9 +0,0 @@
-import { router } from '../trpc';
-import { roomRouter } from './room';
-
-export const appRouter = router({
-  room: roomRouter
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
