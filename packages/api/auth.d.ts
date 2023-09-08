1698570a85e399c58861411c3cff4b8474d8a561
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-09-08 20:20:00 -0300

    fix: auth column name

--- packages/api/auth.d.ts
+++ packages/api/auth.d.ts
@@ -3,7 +3,7 @@ declare namespace Lucia {
   type Auth = import('./src/auth/lucia').Auth;
   type DatabaseUserAttributes = {
     name: string | null;
-    lastName: string | null;
+    last_name: string | null;
     email: string | null;
     username: string;
   };
