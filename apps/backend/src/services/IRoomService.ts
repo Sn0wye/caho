c4502a143c4baa5bec15f696ab50c7b219edae31
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-09-16 15:30:44 -0300

    refactor: tiny inputs

--- apps/backend/src/services/IRoomService.ts
+++ apps/backend/src/services/IRoomService.ts
@@ -1,4 +1,3 @@
-import { type EndRoom, type StartRoom } from '@caho/contracts';
 import { type Player, type Ranking, type Room } from '@caho/schemas';
 import { type CreateRoomInput } from '@/schemas/create-room';
 import { type JoinRoomInput } from '@/schemas/join-room';
@@ -9,8 +8,9 @@ export interface IRoomService {
   createRoom(room: CreateRoomInput): Promise<Room>;
   listPublicRooms(): Promise<Room[]>;
   addPlayerToRoom(input: { roomCode: string; player: Player }): Promise<void>;
-  startRoom(input: StartRoom): Promise<void>;
-  endRoom(input: EndRoom): Promise<Ranking>;
+  startRoom(roomCode: string): Promise<void>;
+  endRoom(roomCode: string): Promise<Ranking>;
   joinRoom(input: JoinRoomInput): Promise<void>;
   leaveRoom(input: LeaveRoomInput): Promise<void>;
+  getRoomPlayers(roomCode: string): Promise<Player[]>;
 }
