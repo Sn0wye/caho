3dc9b469cb53dbd7489e6927d11503d32d5d4052
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-12-14 20:44:09 -0300

    feat: redis singleton

--- apps/backend/src/db/redis.ts
+++ apps/backend/src/db/redis.ts
@@ -1,3 +1,11 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis();
+declare global {
+  // eslint-disable-next-line no-var
+  var redis: Redis | undefined;
+}
+export const redis = global.redis || new Redis();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.redis = redis;
+}
