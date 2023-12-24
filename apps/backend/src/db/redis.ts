d8be0b15d241e08707ccd9c6a64daa877671156f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-12-23 21:04:19 -0300

    refactor: redis env

--- apps/backend/src/db/redis.ts
+++ apps/backend/src/db/redis.ts
@@ -1,10 +1,11 @@
 import Redis from 'ioredis';
+import { env } from '@/env';
 
 declare global {
   // eslint-disable-next-line no-var
   var redis: Redis | undefined;
 }
-export const redis = global.redis || new Redis();
+export const redis = global.redis || new Redis(env.REDIS_URL);
 
 if (process.env.NODE_ENV !== 'production') {
   global.redis = redis;
