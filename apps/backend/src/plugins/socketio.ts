577ac2adb33302da50a23c5525beb008b6cb1328
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2024-02-21 22:57:23 -0300

    wip: what i have now

--- apps/backend/src/plugins/socketio.ts
+++ apps/backend/src/plugins/socketio.ts
@@ -1,8 +1,9 @@
-import { type Player, type Room } from '@caho/schemas';
+import { type Player, type Room, type RoomState } from '@caho/schemas';
+import { type Session, type User } from 'lucia';
 import { Server, type ServerOptions } from 'socket.io';
 import { type App } from '@/app';
-import { auth } from '@/auth/lucia';
-import basePack, { type WhiteCard } from '@/cards/base-pack';
+import { getSessionFromToken } from '@/auth/lucia';
+import basePack, { type BlackCard, type WhiteCard } from '@/cards/base-pack';
 import { HTTPError } from '@/errors/HTTPError';
 import { RedisRoomRepository } from '@/repositories/room';
 import { CardService } from '@/services/CardService';
@@ -15,76 +16,101 @@ export const fastifySocketIO = async (
   opts: Partial<ServerOptions>
 ) => {
   app.decorate('io', new Server(app.server, opts));
-  app.addHook('onClose', (fastify: App, done) => {
+  app.addHook('onClose', (fastify, done) => {
     fastify.io.close();
     done();
   });
 
+  // auth middleware
+  app.io.use(async (socket, next) => {
+    const token = socket.handshake.auth.token;
+
+    if (!token) {
+      return next(new Error('Unauthorized'));
+    }
+
+    const { session, user } = await getSessionFromToken(token);
+
+    if (!session || !user) {
+      return next(new Error('Unauthorized'));
+    }
+
+    socket.data.session = session;
+    socket.data.user = user;
+
+    next();
+  });
+
   app.io.on('connection', async socket => {
-    const roomCode = socket.handshake.query['roomCode']?.toString();
+    const roomCode = socket.data.roomCode;
 
-    if (!roomCode) {
-      socket.emit('message', 'no room code provided');
-      socket.disconnect();
-      return;
-    }
+    socket.on('joinRoom', async (roomCode, password) => {
+      const player = await getOrCreatePlayer({
+        roomCode,
+        user: socket.data.user,
+        redis: app.redis
+      });
 
-    const cookie = socket.request.headers['auth_session']?.toString();
-    if (!cookie) {
-      socket.emit('message', 'no cookie provided');
-      socket.disconnect();
-      return;
-    }
-    const session = await auth.validateSession(cookie);
+      try {
+        await roomService.joinRoom({
+          roomCode,
+          password,
+          player
+        });
+
+        socket.join(roomCode);
+        socket.join(player.id);
+        socket.to(roomCode).emit('playerJoined', player);
+        // TODO: remove this message
+        socket.emit('message', `You have joined the room ${roomCode}.`);
+
+        socket.data.roomCode = roomCode;
+        socket.data.player = player;
+      } catch (e) {
+        console.log(e);
+        if (e instanceof HTTPError) {
+          socket.emit('error', e);
+        }
+      }
+    });
+
     const roomService = new RoomService(new RedisRoomRepository(app.redis));
     const cardService = new CardService(basePack);
-
-    const player = await getOrCreatePlayer({
-      roomCode,
-      session,
-      redis: app.redis
-    });
-
-    socket.join(roomCode);
-    socket.join(player.id);
-    socket.to(roomCode).emit('playerJoined', player);
-
-    socket.on('message', msg => {
-      if (!roomCode) return;
-      socket.to(roomCode).emit('message', msg);
-    });
 
     socket.on('ping', () => {
       socket.emit('message', 'pong');
     });
 
-    // socket.on('joinRoom', async code => {
-    //   socket.join(code);
+    socket.on('message', msg => {
+      socket.to(roomCode).emit('message', msg);
+    });
 
-    //   player = await getOrCreatePlayer({
-    //     roomCode,
-    //     session,
-    //     redis: app.redis
-    //   });
+    socket.on('isReady', async isReady => {
+      const player = socket.data.player;
 
-    //   socket.to(code).emit('playerJoined', player);
-    // });
-
-    socket.on('leaveRoom', () => {
-      if (!roomCode) return;
-      socket.leave(roomCode);
-
-      if (player) {
-        socket.to(roomCode).emit('playerLeft', player.id);
-      }
+      await roomService.updatePlayerInRoom(roomCode, player.id, { isReady });
+      socket.data.player.isReady = isReady;
+      socket.to(roomCode).emit('playerReady', player.id, isReady);
     });
 
     socket.on('startRoom', async () => {
+      console.log(socket.data.player);
+      if (!socket.data.player) {
+        return;
+      }
+
+      if (!socket.data.player.isHost) {
+        console.error('You cannot start, as you are not the host');
+        return;
+      }
+
       try {
         await roomService.startRoom(roomCode);
         const room = await roomService.getRoom(roomCode);
         socket.to(roomCode).emit('roomUpdated', room);
-        setNextJudge(room, roomService);
+
+        const roomState = await roomService.getRoomState(roomCode);
+        await setNextJudge(roomCode, roomState, roomService);
 
         // give players new cards
         const players = await roomService.getRoomPlayers(room.code);
@@ -96,6 +122,8 @@ export const fastifySocketIO = async (
             cards
           });
         }
+        const blackCard = cardService.getNewBlackCards(1)[0];
+        socket.to(roomCode).emit('newBlackCard', blackCard);
       } catch (e) {
         if (e instanceof HTTPError) {
           socket.emit('error', e);
@@ -103,18 +131,22 @@ export const fastifySocketIO = async (
       }
     });
 
-    // socket.on("")
+    socket.on('playCards', cards => {
+      // TODO
+      return;
+    });
 
-    socket.on('isReady', isReady => {
-      if (!roomCode || !player) return;
-      player.isReady = isReady;
+    socket.on('leaveRoom', () => {
+      const player = socket.data.player;
+      socket.leave(roomCode);
 
-      socket.to(roomCode).emit('playerReady', player.id, isReady);
+      if (player) {
+        socket.to(roomCode).emit('playerLeft', player.id);
+      }
     });
 
     socket.on('disconnect', async () => {
-      if (!roomCode) return;
-
+      const player = socket.data.player;
       if (player) {
         socket.to(roomCode).emit('playerLeft', player.id);
       }
@@ -129,20 +161,36 @@ interface ServerToClientEvents {
   playerLeft: (playerId: string) => void;
   playerReady: (playerId: string, isReady: boolean) => void;
   roomUpdated: (room: Room) => void;
+  roomStateUpdated: (roomState: RoomState) => void;
   newCards: (cards: WhiteCard[]) => void;
+  newBlackCard: (card: BlackCard) => void;
 }
 
 interface ClientToServerEvents {
   message: (data: string) => void;
   ping: () => void;
-  joinRoom: (roomCode: string) => void;
+  joinRoom: (roomCode: string, password: string | null) => void;
   leaveRoom: () => void;
   isReady: (isReady: boolean) => void;
   startRoom: () => void;
-  // getCard: (count?: number) => WhiteCard;
+  playCards: (cards: WhiteCard[]) => void;
 }
 
-export type IO = Server<ClientToServerEvents, ServerToClientEvents>;
+interface InterServerEvents {}
+
+interface SocketData {
+  session: Session;
+  user: User;
+  roomCode: string;
+  player: Player;
+}
+
+export type IO = Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>;
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -150,16 +198,20 @@ declare module 'fastify' {
   }
 }
 
-export async function setNextJudge(room: Room, roomService: IRoomService) {
-  const prevJudgeId = room.state.prevJudgeId;
+export async function setNextJudge(
+  roomCode: string,
+  roomState: RoomState,
+  roomService: IRoomService
+) {
+  const prevJudgeId = roomState.prevJudgeId;
 
   if (prevJudgeId) {
-    await roomService.updatePlayerInRoom(room.code, prevJudgeId, {
+    await roomService.updatePlayerInRoom(roomCode, prevJudgeId, {
       isJudge: false
     });
   }
 
-  const players = await roomService.getRoomPlayers(room.code);
+  const players = await roomService.getRoomPlayers(roomCode);
   const playersWithoutJudge = players.filter(
     player => player.id !== prevJudgeId
   );
@@ -167,7 +219,7 @@ export async function setNextJudge(room: Room, roomService: IRoomService) {
   const randomIndex = Math.floor(Math.random() * playersWithoutJudge.length);
   const newJudge = playersWithoutJudge[randomIndex];
 
-  await roomService.updatePlayerInRoom(room.code, newJudge.id, {
+  await roomService.updatePlayerInRoom(roomCode, newJudge.id, {
     isJudge: true
   });
 }
