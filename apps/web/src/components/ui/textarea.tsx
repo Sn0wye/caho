e69cf4d1df3fe2075fdac30b068c9cd6f654ffa0
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-06-03 21:25:09 -0300

    refactor: textarea

--- apps/web/src/components/ui/textarea.tsx
+++ apps/web/src/components/ui/textarea.tsx
@@ -1,23 +1,18 @@
-import * as React from 'react';
+import type * as React from 'react';
 import { cn } from '@/utils/cn';
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export interface TextareaProps extends React.ComponentProps<'textarea'> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <textarea
-        className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-zinc-200 border-zinc-200 bg-transparent px-3 py-2 text-sm ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:border-zinc-800 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-800',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
+const Textarea = ({ className, ...props }: TextareaProps) => (
+  <textarea
+    className={cn(
+      'flex min-h-[80px] w-full rounded-md border border-zinc-200  bg-transparent px-3 py-2 text-sm ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-800',
+      className
+    )}
+    {...props}
+  />
 );
+
 Textarea.displayName = 'Textarea';
 
 export { Textarea };
