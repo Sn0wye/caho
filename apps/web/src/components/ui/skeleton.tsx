54496ee724e70650a30e0c309101631c4fc0ce33
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-06-03 21:24:55 -0300

    refactor: skeleton

--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -1,9 +1,6 @@
 import { cn } from '@/utils/cn';
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       className={cn('animate-pulse bg-zinc-200 dark:bg-zinc-900', className)}
