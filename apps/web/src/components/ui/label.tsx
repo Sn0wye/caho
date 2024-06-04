0ae0d381862e19423dae5c2a204ff568a7c306eb
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-06-03 21:24:29 -0300

    refactor: label

--- apps/web/src/components/ui/label.tsx
+++ apps/web/src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import * as React from 'react';
+import type * as React from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
@@ -9,17 +9,13 @@ const labelVariants = cva(
   'font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
 );
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-));
+const Label = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root> &
+  VariantProps<typeof labelVariants>) => (
+  <LabelPrimitive.Root className={cn(labelVariants(), className)} {...props} />
+);
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
