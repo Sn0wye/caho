5fcc0104762f0943c6b3db69f3a7e5c941d33282
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-06-23 15:32:56 -0300

    chore: update env

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -4,17 +4,13 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']),
-    UPSTASH_REDIS_REST_URL: z.string().url(),
-    UPSTASH_REDIS_REST_TOKEN: z.string().min(1)
+    NODE_ENV: z.enum(['development', 'test', 'production'])
   },
   client: {
     NEXT_PUBLIC_BACKEND_URL: z.string().url()
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION
