3f8d0aecafcf00b378eda3381db6b7c5e2f328c6
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-07-06 17:46:38 -0300

    ci: update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 IMAGE_NAME="${1}"
 GITHUB_SHA="${2}"
-DOCKERHUB_USERNAME="${3}"
+DOCKER_REGISTRY="${3}"
 
-# Pull the new Docker image
-docker pull "${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${GITHUB_SHA}"
+# Pull image from GitHub Container Registry
+docker pull "${DOCKER_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA}"
 
 # Determine new and old container names and ports
 if docker ps | grep -q "caho-api-v1"; then
@@ -23,7 +23,7 @@ fi
 # Run the new container
 docker run -d --name "${new_container}" \
   --env-file ~/caho.env \
-  -p "${new_port}:8080" "${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${GITHUB_SHA}"
+  -p "${new_port}:8080" "${DOCKER_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA}"
 
 # Wait for the new container to start up
 sleep 10
