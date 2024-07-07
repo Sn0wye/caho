2feb37bf6f21fc363c439addffc93f144403f411
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-07-07 14:32:41 -0300

    ci: lets debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,13 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-IMAGE_REFERENCE="${1}"
+IMAGE_REFERENCE=$1
+
+echo "IMAGE_REFERENCE: $IMAGE_REFERENCE"
 
 # Pull the new Docker image from GitHub Container Registry
-docker pull "${IMAGE_REFERENCE}"
+echo "Pulling Docker image: $IMAGE_REFERENCE"
+docker pull $IMAGE_REFERENCE
 
 # Determine new and old container names and ports
 if docker ps | grep -q "caho-api-v1"; then
@@ -18,17 +21,29 @@ else
   new_container="caho-api-v1"
 fi
 
+echo "Old container: $old_container"
+echo "New container: $new_container"
+echo "New port: $new_port"
+
 # Run the new container
-docker run -d --name "${new_container}" \
+echo "Running new container: $new_container"
+docker run -d --name $new_container \
   --env-file ~/caho.env \
-  -p "${new_port}:8080" "${IMAGE_REFERENCE}"
+  -p $new_port:8080 $IMAGE_REFERENCE
 
 # Wait for the new container to start up
+echo "Waiting for the new container to start up..."
 sleep 10
 
 # Stop and remove the old container
-docker stop "${old_container}"
-docker rm "${old_container}"
+echo "Stopping old container: $old_container"
+docker stop $old_container
+
+echo "Removing old container: $old_container"
+docker rm $old_container
 
 # Reload Nginx to update the upstream list
+echo "Reloading Nginx"
 sudo systemctl reload nginx
+
+echo "Deployment completed successfully."
